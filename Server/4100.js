5fcdfc1891f3b517d01f4ebec1083548d3cfcbaf
Kevin Ryser <kevin.ryser@koweb.ch>
2017-05-11 15:42:48 +0100

    Packet 4100 update. Not finished.

--- Server/4100.js
+++ Server/4100.js
@@ -1,6 +1,6 @@
 function ReadPacket()
 {
-	// goN
+	// -- CharacterInformation - 26
 	packet.ReadShort("Size");
 
 	packet.ReadByte("CharacterPartId");
@@ -21,14 +21,32 @@ function ReadPacket()
 		packet.ReadShort("z");
 		packet.ReadShort("instanceId");
 	}
+	// --
 
 	// m_serializedProtectors
-	var pSize = packet.ReadShort("m_serializedProtectors byte Size");
-	packet.ReadShort("nbProtectors");
+	packet.ReadShort("m_serializedProtectors byte Size");
+	var nbProtectors = packet.ReadShort("nbProtectors");
+	for (var i = 0; i < nbProtectors; ++i)
+	{
+		var size = packet.ReadShort("protector serialized size");
+		packet.Skip(size);
+	}
 
 	// m_serializedProtectorInfos
 	packet.ReadShort("serializedProtectorInfos");
-	packet.ReadShort("nbProtectorsInfos");
+	var nbProtectorsInfos = packet.ReadShort("nbProtectorsInfos");
+	for (var i = 0; i < nbProtectorsInfos; ++i)
+	{
+		// this.jdField_b_of_type_JavaUtilArrayList.add(eDh.a((ByteBuffer)localObject));
+		packet.ReadInt("m_id");
+		packet.ReadInt("m_nation");
+		packet.ReadBool("m_isChaos");
+		packet.ReadInt("m_cash");
+		packet.ReadFloat("m_fleaTaxValue");
+		packet.ReadFloat("m_marketTaxValue");
+		packet.ReadInt("m_currentSatisfaction");
+		packet.ReadInt("m_currentSatisfaction");
+	}
 
 	// Dungeon.proto Protobuf
 	var protoBuildSize = packet.ReadShort("dungeon.proto size");
