23744393fac0f55fa87733166eac25e3bedfa998
Kevin Ryser <kevin.ryser@koweb.ch>
2017-06-09 16:23:38 +0100

    Packet 8120 update. Still need more work.

--- Server/8120.js
+++ Server/8120.js
@@ -1,21 +1,48 @@
 function ReadPacket()
 {
-	packet.ReadInt("unk");
-	packet.ReadInt("unk");
-	packet.ReadInt("unk");
-	packet.ReadByte("unk");
-	packet.ReadInt("unk");
+	packet.ReadInt("fightId");
+	packet.ReadInt("uniqueId");
+	packet.ReadInt("triggeringActionUniqueId");
+	packet.ReadBool("triggered");
+	packet.ReadInt("runningEffectId");
 
-	var size = packet.ReadShort("unk");
-		for (var i = 0; i < size; ++i)
-			packet.ReadByte();
+	// baseImpl/common/clientAndServer/game/effect/runningEffect/RunningEffect.java
+	var serializedRunningEffect = packet.ReadShort("serializedRunningEffect size");
+	var blocks = packet.ReadByte("block length");
+
+	for (var i = 0; i < blocks; ++i)
+	{
+		packet.ReadByte("index");
+		packet.ReadInt("offset");
+	}
+
+	for (var i = 0; i < blocks; ++i)
+	{
+		packet.ReadByte("index");
+
+		packet.ReadLong("getUniqueId");
+		packet.ReadLong("getBaseUid");
+		packet.ReadInt("getEffectId");
+		packet.ReadInt("m_targetCell.getX");
+		packet.ReadInt("m_targetCell.getY");
+		packet.ReadShort("m_targetCell.getZ");
+		packet.ReadInt("value");
+	}
+
+	/*
 
 	while (packet.Length() > 0)
 	{
-		var size = packet.ReadShort("short size");
-		for (var i = 0; i < size; ++i)
-			packet.ReadByte(i);
-	}
+		var serializedTarget = packet.ReadShort("serializedTarget");
+		packet.ReadByte("length");
+		packet.ReadByte("index");
+		packet.ReadInt("offset");
+
+		packet.ReadLong("targetId");
+
+		packet.ReadInt("actionId");
+		packet.ReadInt("triggerActionUniqueId");
+	}*/
 }
 
 ReadPacket();
